50263ef0535d3cf9f6173d302e87db00f58f7883
felixx0319 <144153290+felixx0319@users.noreply.github.com>
2025-11-13 22:21:10 +0800

    Add tests for player name setup functionality

--- test/Controller/player_nameTest.java
+++ test/Controller/player_nameTest.java
@@ -1,16 +1,45 @@
 package Controller;
 
 import org.junit.jupiter.api.Test;
-
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.*;
 
 class player_nameTest {
 
     @Test
-    void setupPlayerNames() {
+    void setupPlayerNamesByDefault() {
+        //input the user name by default
+        String userInput = "1\n";
+        ByteArrayInputStream userKeyboard = new ByteArrayInputStream(userInput.getBytes());
+        Scanner scanner = new Scanner(userKeyboard);
+        player_name.setupPlayerNames(scanner);
     }
 
     @Test
-    void random_name() {
+    void setupPlayerNamesByRandom() {
+        //input the user name by random
+        String userInput = "2\n";
+        ByteArrayInputStream userKeyboard = new ByteArrayInputStream(userInput.getBytes());
+        Scanner scanner = new Scanner(userKeyboard);
+        player_name.setupPlayerNames(scanner);
+    }
+
+    @Test
+    void setupPlayerNamesByPlayer() {
+        //input the user name by user themselves
+        String userInput = "3\nPLAYER111\nPLAYER222";
+        ByteArrayInputStream userKeyboard = new ByteArrayInputStream(userInput.getBytes());
+        Scanner scanner = new Scanner(userKeyboard);
+        player_name.setupPlayerNames(scanner);
+    }
+
+    @Test
+    void setupPlayerNamesByDefaultWithWrongInput() {
+        //user input wrong input
+        String userInput = "wrongInput\n1\n";
+        ByteArrayInputStream userKeyboard = new ByteArrayInputStream(userInput.getBytes());
+        Scanner scanner = new Scanner(userKeyboard);
+        player_name.setupPlayerNames(scanner);
     }
 }
