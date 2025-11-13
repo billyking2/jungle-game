529bf68f03a3e017b3b666a0dcc04f401c08bf29
felixx0319 <144153290+felixx0319@users.noreply.github.com>
2025-11-13 23:10:09 +0800

    Update display_mapTest.java

--- test/View/display_mapTest.java
+++ test/View/display_mapTest.java
@@ -1,28 +1,29 @@
 package View;
 
+import Model.chess;
+import Model.map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class display_mapTest {
 
+    private display_map myDisplayMap;
+    private map myMap;
+
+    @BeforeEach
+    void setup(){
+        myMap = new map();
+        myDisplayMap = new display_map(myMap);
+    }
+
     @Test
     void display_map() {
-    }
+        //display the map
+        //make a chess to unknown chess
+        myMap.setChess(0,0,new chess("unknown",1,1,1));
+        myDisplayMap.display_map();
 
-    @Test
-    void colour_chess() {
-    }
-
-    @Test
-    void colour_trap() {
-    }
-
-    @Test
-    void colour_den() {
-    }
-
-    @Test
-    void colour_river() {
     }
 }
