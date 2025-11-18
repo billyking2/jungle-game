0f978be4d196b3984ee9d47e6a92a92bcbaa6905
felixx0319 <144153290+felixx0319@users.noreply.github.com>
2025-11-18 23:39:13 +0800

    Update game_controlTest.java

--- test/Controller/game_controlTest.java
+++ test/Controller/game_controlTest.java
@@ -163,42 +163,70 @@ class game_controlTest {
 
     @Test
     void startGame5() throws IOException{
-        //test the startGame method with winning condition and save
+        //test the startGame method with winning condition （player2 get into the dens) and save
         String[] player_name = {"1","2"};
-        String userInput = "undo\n" +
+        String userInput = "undo\n" + //test undo at the first step
+                "help\n" + //test the help command
+                "tiger\n" +
+                "back\n" +  //test the back command
+                "invalidName\n" + //test wrong animal name input
                 "wolf\n" +
-                "right\n" +
-                "cat\n" +
-                "back\n" +
-                "WrongName\n" +
-                "wolf\n" +
-                "wrongDirection\n" +
-                "right\n" +
-                "wolf\n" +
-                "left\n" +
-                "wolf\n" +
+                "left\n" + //test invalid move
+                "rat\n" +
+                "display\n" + //test the display
+                "invalidMove\n" + //test invalid move
                 "up\n" +
-                "wolf\n" +
-                "right\n" +
-                "wolf\n" +
+
+                "wolf \n" +
+                "down\n" +
+                "rat\n" +
                 "up\n" +
-                "wolf\n" +
-                "left\n" +
-                "wolf\n" +
+                "undo\n"+ //player 1 undo
+                "wolf \n" +
+                "down\n" +
+                "rat\n" +
                 "up\n" +
-                "wolf\n" +
-                "right\n" +
-                "wolf\n" +
+                "undo\n"+
+                "wolf \n" +
+                "down\n" +
+                "rat\n" +
                 "up\n" +
+                "undo\n"+
+                "wolf \n" +
+                "down\n" +
+                "rat\n" +
+                "up\n" +
+                "undo\n"+ //test using all undo
+
+
                 "wolf\n" +
-                "left\n" +
-                "wolf\n" +
+                "DOWN\n" +// test uppercase letter
+                "rat\n" +
                 "up\n" +
                 "wolf\n" +
                 "down\n" +
-                "wolf\n" +
+                "rat\n" +
                 "up\n" +
-                "yes\n";
+                "wolf\n" +
+                "left\n" +
+                "down\n" +
+
+                "rat\n" +
+                "left\n" +
+                "wolf\n" +
+                "down\n" +
+                "undo\n" + //player 2 undo
+                "rat\n" +
+                "left\n" +
+                "wolf\n" +
+                "down\n" +
+
+                "rat\n" +
+                "right\n" +
+                "wolf\n" +
+                "down\n" +
+                "yes\n" +
+                "Rfile2\n";
         ByteArrayInputStream userKeyboard = new ByteArrayInputStream(userInput.getBytes());
         Scanner scanner = new Scanner(userKeyboard);
         game_control my_game_control = new game_control();
@@ -210,60 +238,46 @@ class game_controlTest {
         Path path = Paths.get("././record/Rfile1.record");
         Files.deleteIfExists(path);
 
+
         try{
             my_game_control.startGame(player_name,scanner,record_name,jungle_file_name,continue_record);
         }
         catch (NoSuchElementException e){
 
         }
+        Path path2 = Paths.get("././record/Rfile2.record");
+        Files.deleteIfExists(path2);
     }
 
     @Test
     void startGame6() throws IOException{
-        //test the startGame method with winning condition and dont save
+        //test the startGame method with winning condition (player 2 get into the dens) and dont save
         String[] player_name = {"1","2"};
-        String userInput = "wolf\n" +
-                "right\n" +
+        String userInput ="rat\n" +
+                "down\n" +
                 "wolf\n" +
                 "right\n" +
+                "rat\n" +
+                "down\n" +
                 "wolf\n" +
-                "back\n"+
-
+                "up\n" +
+                "rat\n" +
+                "down\n" +
                 "wolf\n" +
-                "display\n" +
+                "up\n" +
+                "rat\n" +
+                "down\n" +
+                "wolf\n" +
+                "up\n" +
+                "rat\n" +
+                "right\n" +
+                "wolf\n" +
+                "up\n" +
+                "rat\n" +
                 "left\n" +
                 "wolf\n" +
                 "up\n" +
-                "wolf\n" +
-                "right\n" +
-                "wolf\n" +
-                "up\n" +
-                "wolf\n" +
-                "left\n" +
-                "wolf\n" +
-                "up\n" +
-
-                "wolf\n" +
-                "right\n" +
-                "wolf\n" +
-                "up\n" +
-                "undo\n" +
-                "wolf\n" +
-                "right\n" +
-                "wolf\n" +
-                "up\n" +
-
-                "wolf\n" +
-                "left\n" +
-                "wolf\n" +
-                "up\n" +
-                "undo\n" +
-                "wolf\n" +
-                "left\n" +
-                "wolf\n" +
-                "up\n" +
-
-                "wolf\n" +
+                "rat\n" +
                 "right\n" +
                 "wolf\n" +
                 "up\n" +
@@ -286,6 +300,157 @@ class game_controlTest {
 
         }
     }
+
+    @Test
+    void startGame7() throws IOException{
+        //test the startGame method with winning condition (player 1 eat all the chess) and dont save
+        String[] player_name = {"1","2"};
+
+        String userInput =  "rat\nup\n" +
+                "elephant\nup\n" +
+                "tiger\nleft\n" +
+                "elephant\nup\n" +
+                "leopard\nleft\n" +
+                "elephant\nup\n" +
+                "lion\nright\n" +
+                "elephant\nup\n" +
+                "elephant\nleft\n" +
+                "elephant\nright\n" +
+                "wolf\nleft\n" +
+                "elephant\nup\n" +
+                "tiger\nleft\n" +
+                "elephant\nleft\n" + //elephant cant eat rat
+                "elephant\nup\n" +
+                "tiger\ndown\n" +
+                "elephant\nright\n" +
+                "rat\ndown\n" +
+                "elephant\ndown\n" +
+                "rat\ndown\n" +
+                "elephant\nright\n" +
+                "rat\ndown\n" +
+                "elephant\ndown\n" +
+                "rat\ndown\n" +
+                "elephant\nright\n" +
+                "rat\ndown\n" +
+                "elephant\nup\n" +
+                "rat\ndown\n" +
+                "elephant\nright\n" +
+                "rat\nup\n" +
+                "elephant\ndown\n" +
+                "rat\ndown\n" +
+                "cat\nleft\n" +
+                "no\n";
+        ByteArrayInputStream userKeyboard = new ByteArrayInputStream(userInput.getBytes());
+        Scanner scanner = new Scanner(userKeyboard);
+        game_control my_game_control = new game_control();
+        String record_name = null;
+        String jungle_file_name = null;
+        boolean continue_record = false;
+
+        //delete Rfile1 if it exist
+        Path path = Paths.get("././record/Rfile1.record");
+        Files.deleteIfExists(path);
+
+        try{
+            my_game_control.startGame(player_name,scanner,record_name,jungle_file_name,continue_record);
+        }
+        catch (NoSuchElementException e){
+
+        }
+    }
+
+    @Test
+    void startGame8() throws IOException{
+        //test the startGame method with winning condition (player 1 eat all the chess) and dont save
+        String[] player_name = {"1","2"};
+
+        String userInput =  "elephant\ndown\n" +
+                "rat\ndown\n" +
+                "elephant\ndown\n" +
+                "tiger\nright\n" +
+                "elephant\ndown\n" +
+                "elephant\nright\n" +
+                "elephant\ndown\n" +
+                "leopard\nright\n" +
+                "elephant\nleft\n" +
+                "lion\nleft\n" +
+                "elephant\ndown\n" +
+                "rat\nup\n" +
+                "elephant\ndown\n" +
+                "tiger\nright\n" +
+                "elephant\nleft\n" +
+                "tiger\nup\n" +
+                "elephant\nup\n" +
+                "rat\nup\n" +
+                "elephant\nleft\n" +
+                "rat\nup\n" +
+                "elephant\nleft\n" +
+                "rat\nup\n" +
+                "elephant\nleft\n" +
+                "rat\nup\n" +
+                "elephant\nup\n" +
+                "rat\nup\n" +
+                "elephant\nright\n" +
+                "rat\ndown\n" +
+                "wolf\nright\n" +
+                "rat\nup\n" +
+                "cat\nright\n" +
+                "no\n";
+        ByteArrayInputStream userKeyboard = new ByteArrayInputStream(userInput.getBytes());
+        Scanner scanner = new Scanner(userKeyboard);
+        game_control my_game_control = new game_control();
+        String record_name = null;
+        String jungle_file_name = null;
+        boolean continue_record = false;
+
+        //delete Rfile1 if it exist
+        Path path = Paths.get("././record/Rfile1.record");
+        Files.deleteIfExists(path);
+
+        try{
+            my_game_control.startGame(player_name,scanner,record_name,jungle_file_name,continue_record);
+        }
+        catch (NoSuchElementException e){
+
+        }
+    }
+
+
+
+    @Test
+    void ask_save_file() throws IOException{
+        //test the save file
+        String[] player_name = {"1","2"};
+        String userInput = "Jfile4";
+        ByteArrayInputStream userKeyboard = new ByteArrayInputStream(userInput.getBytes());
+        Scanner scanner = new Scanner(userKeyboard);
+        game_file.FileType fileType = game_file.FileType.JUNGLE;
+        try {
+            my_game_control.ask_save_file(scanner, player_name, fileType);
+        }
+        catch (NoSuchElementException e){
+        }
+    }
+
+    @Test
+    void ask_save_file2() throws IOException{
+        //test the save file
+        String[] player_name = {"1","2"};
+        String userInput = "Jfile5";
+        ByteArrayInputStream userKeyboard = new ByteArrayInputStream(userInput.getBytes());
+        Scanner scanner = new Scanner(userKeyboard);
+        game_file.FileType fileType = game_file.FileType.JUNGLE;
+        //delete Rfile1 if it exist
+        Path path = Paths.get("././jungle/Jfile5.record");
+        Files.deleteIfExists(path);
+        try {
+            my_game_control.ask_save_file(scanner, player_name, fileType);
+        }
+        catch (NoSuchElementException e){
+        }
+    }
+
+
 
 
 }
